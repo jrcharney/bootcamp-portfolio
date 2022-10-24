c924727fdb62ad421a55bfaa359305b32607605f
Jason Charney <jrcharney@gmail.com>
2022-10-24 11:27:46 -0500

    Fixed a visibility issue with the top button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 /* File: assets/js/script.js */
 
-/* TODO: animate the scrolling */
+// I don't know why I couldn't set the visibility of the top link in CSS, so I had to forcce it by setting the HTML attribute.
 const nav = document.querySelector("nav");
-const navInitOffsetTop = nav.offsetTop;
-
-window.addEventListener('scroll', (event) => {
+const topLink = document.querySelector(".navmenu > ul > li:last-child");
+window.addEventListener('scroll', () => {
     if(window.scrollY >= nav.offsetTop){
         nav.classList.add("sticky");
+        topLink.setAttribute("style",'visibility:visibile;')
     }else{
         nav.classList.remove("sticky");
+        topLink.setAttribute("style",'visibility:hidden;')
     }
 });
